bdf910a13d476f0b9dc3b914a888fe1a08c8f2dd
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2017-09-07 00:24:22 +0300

    Add an explanation

--- app/src/main/java/ru/maksim/hardware_acceleration/MainActivity.java
+++ app/src/main/java/ru/maksim/hardware_acceleration/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // If android:hardwareAccelerated="false" is set to this activity, both CustomView and
+        // CustomView2 are redrawn when the delayed task is executed (although CustomView hasn't
+        // changed, it's redrawn because part of it is in the dirty area.
+        // See https://developer.android.com/guide/topics/graphics/hardware-accel.html#hardware-model).
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
